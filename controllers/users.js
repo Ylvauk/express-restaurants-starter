58924b1d84220ec5c05c07028fc758707bb54e12
esin87 <esin.saribudak@gmail.com>
2021-10-04 19:19:46 -0500

    Add restaurants and users many-to-many

--- controllers/users.js
+++ controllers/users.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../models/User');
+const User = require('../models/user');
 
 router.get('/', (req, res, next) => {
 	User.find({})
+		.populate('restaurants')
 		.then((users) => res.json(users))
 		.catch(next);
 });
--- models/restaurant.js
+++ models/restaurant.js
@@ -14,6 +14,7 @@ const restaurantSchema = new mongoose.Schema(
 			required: true,
 		},
 		reviews: [reviewSchema],
+		customers: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
 	},
 	{ timestamps: true }
 );
